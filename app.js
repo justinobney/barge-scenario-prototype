60194655ed2862c8cf05c4a9b7352448faa14e8d
Justin Obney <justinobney@gmail.com>
2013-06-12 20:00:56 -0500

    cleaning up code

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
-'use strict';
+"use strict";
 
 var myLayout;
 
 
 $(document).ready(function(){
 
-	// first set a 'fixed height' on the container so it does not collapse...
-	var $Container = $('#container')
-	$Container.height( $(window).height() - $Container.offset().top );
+    // first set a 'fixed height' on the container so it does not collapse...
+    var $Container = $('#container');
+    $Container.height( $(window).height() - $Container.offset().top );
 
-	// NOW create the layout
-	myLayout = $('#container').layout({
-		defaults: {
-			   fxName:               "slide"
-			,  fxSpeed:               "slow"
-			,  spacing_closed:        14
-			,  spacing_open:        14
-		},
-		north__spacing_open: 0
-	});
+    // NOW create the layout
+    myLayout = $('#container').layout({
+        defaults: {
+               fxName:               "slide"
+            ,  fxSpeed:               "slow"
+            ,  spacing_closed:        14
+            ,  spacing_open:        14
+        },
+        north__spacing_open: 0
+    });
 
-	myLayout.sizePane('east', 250);
-	myLayout.sizePane('west', 350);
-
-	$('.workspace-container').each(function(i,e){
-		if(i % 3 == 0){
-			$(e).addClass('new-row')
-		}
-	})
+    myLayout.sizePane('east', 250);
+    myLayout.sizePane('west', 350);
 });
 
 angular.module('draggableBoxes', ['ngDragDrop']);
 
 angular.module('draggableBoxes').filter('thirds', function() {
   return function(items, position) {
-  	var filtered = [];
+    var filtered = [];
     angular.forEach(items, function(item, idx) {
       if(idx % 3 == position) {
         filtered.push(item);
@@ -45,241 +39,242 @@ angular.module('draggableBoxes').filter('thirds', function() {
 });
 
 (function(){
-	var scenarioController = function($scope, $log){
-		$scope.layout = {
-			dock: {
-				workAreas: [
-			        {
-			            "id": 3,
-			            "title": "Compuamerica",
-			            "units": [
-			                {
-			                    "boat": "Compuamerica",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 4,
-			            "title": "iSkyvaco",
-			            "units": [
-			                {
-			                    "boat": "iSkyvaco",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 5,
-			            "title": "Teratopia",
-			            "units": [
-			                {
-			                    "boat": "Teratopia",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 1,
-			            "title": "iQualcar",
-			            "units": [
-			                {
-			                    "boat": "iQualcar",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 2,
-			            "title": "Fibrotouch",
-			            "units": [
-			                {
-			                    "boat": "Fibrotouch",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        }
-				]
-			},
-			workSpace: {
-				workAreas: [
-					{
-			            "id": 1,
-			            "title": "iQualcar",
-			            "units": [
-			                {
-			                    "boat": "iQualcar",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 2,
-			            "title": "Fibrotouch",
-			            "units": [
-			                {
-			                    "boat": "Fibrotouch",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 3,
-			            "title": "Compuamerica",
-			            "units": [
-			                {
-			                    "boat": "Compuamerica",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 4,
-			            "title": "iSkyvaco",
-			            "units": [
-			                {
-			                    "boat": "iSkyvaco",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 5,
-			            "title": "Teratopia",
-			            "units": [
-			                {
-			                    "boat": "Teratopia",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 1,
-			            "title": "iQualcar",
-			            "units": [
-			                {
-			                    "boat": "iQualcar",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        },
-			        {
-			            "id": 2,
-			            "title": "Fibrotouch",
-			            "units": [
-			                {
-			                    "boat": "Fibrotouch",
-			                    "barges": [
-			                        {
-			                            "name": "barge 1"
-			                        },
-			                        {
-			                            "name": "barge 2"
-			                        }
-			                    ]
-			                }
-			            ]
-			        }
-				]
-			}
-		};
+    var scenarioController = function($scope){
+        $scope.layout = {
+            dock: {
+                workAreas: [
+                    {
+                        "id": 3,
+                        "title": "Compuamerica",
+                        "units": [
+                            {
+                                "boat": "Compuamerica",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 4,
+                        "title": "iSkyvaco",
+                        "units": [
+                            {
+                                "boat": "iSkyvaco",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 5,
+                        "title": "Teratopia",
+                        "units": [
+                            {
+                                "boat": "Teratopia",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 1,
+                        "title": "iQualcar",
+                        "units": [
+                            {
+                                "boat": "iQualcar",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "title": "Fibrotouch",
+                        "units": [
+                            {
+                                "boat": "Fibrotouch",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            workSpace: {
+                workAreas: [
+                    {
+                        "id": 1,
+                        "title": "iQualcar",
+                        "units": [
+                            {
+                                "boat": "iQualcar",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "title": "Fibrotouch",
+                        "units": [
+                            {
+                                "boat": "Fibrotouch",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 3,
+                        "title": "Compuamerica",
+                        "units": [
+                            {
+                                "boat": "Compuamerica",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 4,
+                        "title": "iSkyvaco",
+                        "units": [
+                            {
+                                "boat": "iSkyvaco",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 5,
+                        "title": "Teratopia",
+                        "units": [
+                            {
+                                "boat": "Teratopia",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 1,
+                        "title": "iQualcar",
+                        "units": [
+                            {
+                                "boat": "iQualcar",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "title": "Fibrotouch",
+                        "units": [
+                            {
+                                "boat": "Fibrotouch",
+                                "barges": [
+                                    {
+                                        "name": "barge 1"
+                                    },
+                                    {
+                                        "name": "barge 2"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        };
 
-		$scope.comments = [
-			{ id: 1, name: 'Aaron Landry', comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo' },
-			{ id: 2, name: 'Justin Obney', comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo' }
-		];
+        $scope.comments = [
+            { id: 1, name: 'Aaron Landry', comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo' },
+            { id: 2, name: 'Justin Obney', comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo' }
+        ];
 
-		$scope.addComment = function(){
-			if ($scope.commentBox) {
-				var newId = _.max($scope.comments, function(comment){ return comment.id; }) + 1;
-				var comment = {id: newId, name: 'Bob', comment: $scope.commentBox };
-				$scope.comments.push(comment);
-				$scope.commentBox = '';
-			} else {
-				alert("No comment! That's kinda silly..");
-			}
-		}
-	};
+        $scope.addComment = function(){
+            if ($scope.commentBox) {
+                var newId = _.max($scope.comments, function(comment){ return comment.id; }) + 1;
+                var comment = {id: newId, name: 'Bob', comment: $scope.commentBox };
 
-	scenarioController.$inject = ['$scope', '$log'];
+                $scope.comments.push(comment);
+                $scope.commentBox = '';
+            } else {
+                alert("No comment! That's kinda silly..");
+            }
+        }
+    };
 
-	angular.module('draggableBoxes')
-		.controller('ScenarioController', scenarioController);
+    scenarioController.$inject = ['$scope'];
+
+    angular.module('draggableBoxes')
+        .controller('ScenarioController', scenarioController);
 })();
