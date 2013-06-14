ac14affef949642200c84d75dbb411798296b26a
Justin Obney <justinobney@gmail.com>
2013-06-14 18:52:39 -0500

    complete app restructure

---
+++ app/app.js
@@ -0,0 +1,83 @@
+"use strict";
+
+var myLayout;
+var io_location = 'http://settoon-location-events-1.nodejitsu.com:80';
+
+$(document).ready(function () {
+
+    var socket = io.connect(io_location);
+
+    // first set a 'fixed height' on the container so it does not collapse...
+    var $Container = $('#container');
+    $Container.height($(window).height() - $Container.offset().top);
+
+    // NOW create the layout
+    myLayout = $('#container').layout({
+        defaults: {
+            fxName: "slide",
+            fxSpeed: "slow",
+            spacing_closed: 14,
+            spacing_open: 14
+        },
+        west: {
+            onopen: function(panel){
+                socket.emit('panel-open', panel);
+            },
+            onclose: function(panel){
+                socket.emit('panel-close', panel);
+            }
+        },
+        east: {
+            onopen: function(panel){
+                socket.emit('panel-open', panel);
+            },
+            onclose: function(panel){
+                socket.emit('panel-close', panel);
+            }
+        },
+        south__initClosed: true,
+        togglerLength_open: 150,
+        togglerLength_closed: 150,
+        onload_end: function(){
+            var minHeight = parseInt($('.ui-layout-center').height() * 0.95);
+            $('#content .span4.ui-droppable').css('min-height', minHeight + 'px');
+        }
+    });
+
+    myLayout.sizePane('east', 250);
+    myLayout.sizePane('west', 350);
+    myLayout.sizePane('south', 350);
+
+    socket.on('open-panel', function(panel){
+        myLayout.open(panel);
+    });
+
+    socket.on('close-panel', function(panel){
+        myLayout.close(panel);
+    });
+
+    $(window).unload(function(){ layoutState.save('myLayout') });
+
+    var handleDragStart = function (evt) {
+        evt.stopPropagation();
+        var el = $(this);
+        var objectType = el.data('object-type');
+        $('body').addClass('dragging-' + objectType);
+    };
+
+    var handleDragStop = function (evt) {
+        evt.stopPropagation();
+        $('body').removeClass();
+    };
+
+    jQuery(document).on('dragstart', '.barge, .unit, .workspace-container', handleDragStart);
+
+    jQuery(document).on('drop', handleDragStop);
+
+    setTimeout(function(){
+        $('.ui-draggable').draggable('disable');
+    },1500);
+
+});
+
+angular.module('draggableBoxes', ['ngDragDrop', 'LocalStorageModule','btford.socket-io']);
---
+++ app/controllers.js
@@ -0,0 +1,243 @@
+
+(function () {
+    var scenarioController = function ($scope, localStorageService, socket) {
+        var original = {
+            "dock": {
+                "workAreas": [
+                    {
+                        "id": 4,
+                        "title": "Not Working",
+                        "units": [{
+                            "boat": "Fred",
+                            "barges": []
+                        }]
+                    }, {
+                        "id": 2,
+                        "title": "New Construction",
+                        "units": [{
+                            "boat": "Charlotte",
+                            "barges": []
+                        }, {
+                            "boat": "Dudley",
+                            "barges": []
+                        }]
+                    }, {
+                        "id": 2,
+                        "title": "Unavailable Storage Barges",
+                        "units": [{
+                            "boat": "Megan",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }]
+                        }]
+                    }, {
+                        "id": 1,
+                        "title": "Chevron",
+                        "units": [{
+                            "boat": "Cathy",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }]
+                        }]
+                    }, {
+                        "id": 3,
+                        "title": "Plains",
+                        "units": [{
+                            "boat": "Albert",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }]
+                        }]
+                    }
+                ]
+            },
+            "workSpace": {
+                "workArea1": [
+                    {
+                        "id": 5,
+                        "title": "Valero",
+                        "units": [{
+                            "boat": "Austin",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }]
+                        }]
+                    }, {
+                        "id": 3,
+                        "title": "Spot",
+                        "units": [{
+                            "boat": "Ryan",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }]
+                        }]
+                    }
+                ],
+                "workArea2": [
+                    {
+                        "id": 2,
+                        "title": "Saltwater",
+                        "units": [{
+                            "boat": "Geneveve",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }, {
+                                "name": "barge 2"
+                            }]
+                        }]
+                    }, {
+                        "id": 4,
+                        "title": "Citco",
+                        "units": [{
+                            "boat": "Marry",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }, {
+                                "name": "barge 2"
+                            }, {
+                                "name": "barge 1"
+                            }]
+                        }]
+                    }, {
+                        "id": 5,
+                        "title": "Nigeria",
+                        "units": [{
+                            "boat": "Emily",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }, {
+                                "name": "barge 3"
+                            }]
+                        }]
+                    }
+                ],
+                "workArea3": [
+                    {
+                        "id": 1,
+                        "title": "Shell",
+                        "units": [{
+                            "boat": "Francis",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }, {
+                                "name": "barge 2"
+                            }, {
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 4"
+                            }, {
+                                "name": "barge 1"
+                            }]
+                        }]
+                    }, {
+                        "id": 1,
+                        "title": "Working Storage Barges",
+                        "units": [{
+                            "boat": "Jane P",
+                            "barges": [{
+                                "name": "barge 1"
+                            }, {
+                                "name": "barge 2"
+                            }]
+                        }]
+                    }
+                ]
+            },
+            "id": 1
+        }
+
+        $scope.comments = [{
+            id: 1,
+            name: 'Aaron Landry',
+            comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo'
+        }, {
+            id: 2,
+            name: 'Justin Obney',
+            comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo'
+        }];
+
+        $scope.inControl = false;
+
+        $scope.takeControl = function(){
+            $scope.inControl = true;
+            $('.ui-draggable').draggable('enable');
+        }
+
+        $scope.saveVersion = function () {
+            localStorageService.add('layout', JSON.stringify($scope.layout));
+
+            //socket.emit('data', JSON.stringify(angular.copy($scope.layout)));
+        };
+
+        $scope.loadVersion = function () {
+            if (localStorageService.get('layout')) {
+                $scope.layout = JSON.parse(localStorageService.get('layout'));
+            } else {
+                $scope.layout = original;
+            }
+            $scope.layout = JSON.parse(localStorageService.get('layout'));
+        };
+
+        $scope.resetVersion = function () {
+            $scope.layout = original;
+        };
+
+        $scope.addComment = function () {
+            if ($scope.commentBox) {
+                var newId = _.max($scope.comments, function (comment) {
+                    return comment.id;
+                }) + 1;
+                var comment = {
+                    id: newId,
+                    name: 'Bob',
+                    comment: $scope.commentBox
+                };
+
+                $scope.comments.push(comment);
+                $scope.commentBox = '';
+            } else {
+                alert("No comment! That's kinda silly..");
+            }
+        };
+
+        $scope.$watch('layout', function () {
+            $scope.saveVersion();
+        }, true);
+
+        // Socket.io stuff here...
+
+        // socket.on('data-update', function(data){
+        //     var data = angular.copy(angular.fromJson(data));
+
+        //     $scope.$apply(function(){
+        //         $scope.layout = data;
+        //     });
+        // });
+        // End Socket.io
+
+        $scope.loadVersion();
+    };
+
+    scenarioController.$inject = ['$scope', 'localStorageService', 'socket'];
+
+    angular.module('draggableBoxes')
+        .controller('ScenarioController', scenarioController);
+})();
